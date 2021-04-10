4b2db9eaeb13468d8ff228e073c4c2f083c918bc
yjsoto <79336667+yjsoto@users.noreply.github.com>
2021-04-09 22:11:51 -0500

    Delete src/banco directory

--- src/banco/dominio/Banco.java
+++
@@ -1,7 +0,0 @@
-package banco.dominio;
-
-public class Banco {
-    public String nit;
-    public String nombre;
-    public Persona gerente;
-}
--- src/banco/dominio/Persona.java
+++
@@ -1,8 +0,0 @@
-package banco.dominio;
-
-public class Persona {
-    public int cedula;
-    public String nombre;
-    public String estadoCivil;
-    public String genero;
-}
